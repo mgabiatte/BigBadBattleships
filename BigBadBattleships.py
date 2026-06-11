73e34c502d4b38d5f8565e3057f710f095d7cd63
mgabiatte <mgabiatti@icloud.com>
2026-06-11 20:42:22 -0300

    Pitada de boniteza no código
    Boniteza seca seca, não consegui aplicar o rich como queríamos, e prefirimos manter sem biblioteca rich

--- BigBadBattleships.py
+++ BigBadBattleships.py
@@ -29,10 +29,10 @@ def obterCoordenadaValida(mensagemLinha, mensagemColuna):
                 return linha, coluna
             else:
                 print(
-                    f"Coordenadas inválidas! Digite Linha (0 a {linhas - 1}) e Coluna (0 a {colunas - 1})."
+                    f"⚠️  Coordenadas inválidas, assim vamos atingir Curitiba Senhor! \n-> Digite Linha (0 a {linhas - 1}) e Coluna (0 a {colunas - 1})."
                 )
         except ValueError:
-            print("Por favor, capitão, digite apenas números inteiros!")
+            print("⚠️  Por favor, capitão, digite apenas números inteiros!")
 
 def posicionarNaviosJogador(tabuleiro):
     navios = 0
@@ -40,11 +40,11 @@ def posicionarNaviosJogador(tabuleiro):
     mostrarTabuleiro(tabuleiro)
     
     while navios < quantidadeNavios:
-        print(f"\nOnde devemos posicionar o navio {navios + 1} capitão?")
+        print(f"\n->Onde devemos posicionar o navio {navios + 1}, capitão?")
         linha, coluna = obterCoordenadaValida("Linha: ", "Coluna: ")
         
         if tabuleiro[linha][coluna] == "N":
-            print("Já existe um navio nessa posição!")
+            print("⚠️  Já existe um navio nessa posição, Senhor!")
         else:
             tabuleiro[linha][coluna] = "N"
             navios += 1
@@ -63,7 +63,7 @@ def posicionarNaviosComputador(tabuleiro):
 
 def ataqueJogador(tabuleiroReal, tabuleiroVisivel):
     while True:
-        print("\nSua vez de atacar, capitão!")
+        print("\n-> Nossa vez de atacar, capitão!")
         linha, coluna = obterCoordenadaValida(
             "Linha do ataque: ", "Coluna do ataque: "
         )
@@ -72,16 +72,16 @@ def ataqueJogador(tabuleiroReal, tabuleiroVisivel):
             tabuleiroVisivel[linha][coluna] == "X"
             or tabuleiroVisivel[linha][coluna] == "O"
         ):
-            print("Você já atacou essa coordenada! Escolha outro alvo.")
+            print("⚠️  Já atacamos essa, Senhor! Escolha outra.")
             continue
 
         if tabuleiroReal[linha][coluna] == "N":
-            print("BOA! ALVO ATINGIDO!!!!")
+            print("-> ISSO! ALVO ATINGIDO, SENHOR!!!!")
             tabuleiroVisivel[linha][coluna] = "X"
             tabuleiroReal[linha][coluna] = "X"
             return True
         else:
-            print("ERRAMOS O DISPARO!")
+            print("MALDIÇÃO!! ERRAMOS O DISPARO!")
             tabuleiroVisivel[linha][coluna] = "O"
             tabuleiroReal[linha][coluna] = "O"
             return False
@@ -100,12 +100,12 @@ def ataqueComputador(tabuleiroReal, tabuleiroVisivel):
     print(f"\nComputador atacou ({linha},{coluna})")
 
     if tabuleiroReal[linha][coluna] == "N":
-        print("Computador acertou!")
+        print("Droga!!! o Computador nos acertou, senhor!")
         tabuleiroVisivel[linha][coluna] = "X"
         tabuleiroReal[linha][coluna] = "X"
         return True
     else:
-        print("Computador errou!")
+        print("UFA! o Computador errou!")
         tabuleiroVisivel[linha][coluna] = "O"
         tabuleiroReal[linha][coluna] = "O"
         return False
@@ -118,13 +118,18 @@ def contarNavios(tabuleiro):
                 quantidade += 1
     return quantidade
 # ==============================================================================
+print("\n" + "#" * 45)
+print("           ⚓ BigBadBattleships ⚓          ")
+print("     Primeiro jogo AAAA de Batalha Naval      ")
+print("#" * 45)
+
 tabuleiroJogador = criarTabuleiro()
 tabuleiroComputador = criarTabuleiro()
 
 feedbackJogador = criarTabuleiro()
 feedbackComputador = criarTabuleiro()
 
-print("POSICIONE SEUS NAVIOS")
+print("\nPOSICIONE SEUS NAVIOS")
 posicionarNaviosJogador(tabuleiroJogador)
 posicionarNaviosComputador(tabuleiroComputador)
 
@@ -142,17 +147,16 @@ while True:
     ataqueJogador(tabuleiroComputador, feedbackJogador)
 
     if contarNavios(tabuleiroComputador) == 0:
-        print("\nVENCEMOS CAPITÃO!")
+        print("\nYES!!! VENCEMOS CAPITÃO!!!")
         break
 
     ataqueComputador(tabuleiroJogador, feedbackComputador)
 
     if contarNavios(tabuleiroJogador) == 0:
-        print("\nCOMPUTADOR VENCEU!")
+        print("\nNÃO! COMPUTADOR VENCEU E A HUMANIDADE SUCUMBIU!")
         break
 
 print(
     "\nCriado cabulosamente por: Matheus Gabiatti; Rafael Rautte e Matheus Mariani!"
 )
 print("Obrigado por jogar nosso game!")
-
